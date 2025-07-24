e0466bf51b66d1b25078a9161068d9c931823650
unknown <rafaelcon96@gmail.com>
2025-07-24 11:20:53 +0300

    fix social

--- src/components/ui/Socials/Socials.tsx
+++ src/components/ui/Socials/Socials.tsx
@@ -49,7 +49,7 @@ const FloatingDockMobile = ({
         {open && (
           <motion.div
             layoutId="nav"
-            className="absolute inset-x-0 bottom-full mb-2 flex flex-col gap-2 cursor-pointer pr-4"
+            className="absolute left-0.5 bottom-full mb-2 flex flex-col gap-2 cursor-pointer"
             onMouseLeave={() => setActiveIndex(null)}
           >
             {items.map((item, idx) => (
